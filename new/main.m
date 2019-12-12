e0c214284ad85dac320aa3fe124a5d6012dfb8ed
simonmtr <294simon@googlemail.com>
2019-12-12 12:05:45 +0100

    added or and not

--- new/main.m
+++ new/main.m
@@ -1,22 +1,34 @@
 #import <Foundation/Foundation.h>
-// #import<node.m>
 
-
-void sayHello() {
-    NSLog(@"Hello!");
-}
-
-void sayBye(){
-    NSLog(@"Bye");
-}
-
-void doSomething(void (*something)(void)) {
-    something();
-}
-
-    void (^simpleBlock)(void) = ^{
-        NSLog(@"This is a block");
+    void (^hello)(void) = ^(void) {
+        printf("Hello, block!\n");
     };
+
+BOOL (^parseAND)(BOOL, BOOL)=^BOOL (BOOL a, BOOL b){
+    BOOL c = a&&b;
+    NSLog(c ? @"parse and: Yes" : @"parse and: No");
+    return c;
+};
+
+BOOL (^parseOR)(BOOL, BOOL)=^BOOL (BOOL a, BOOL b){
+    BOOL c = a||b;
+    NSLog(c ? @"parse or: Yes" : @"parse or: No");
+    return c;
+};
+
+BOOL (^parseNot)(BOOL)=^BOOL (BOOL a){
+    BOOL c = !a;
+    NSLog(c ? @"parse not: Yes" : @"parse not: No");
+    return c;
+};
+
+BOOL parseBooleanExpression(){
+    
+    parseNot(true);
+    parseOR(true,true);
+    parseAND(false,true);
+    return true;
+}
 
 int main (int argc, const char * argv[])
 {
@@ -26,9 +38,13 @@ int main (int argc, const char * argv[])
     nodeAnd= [NodeOr alloc];
     [nodeAnd Eval:nanl];
  */ 
-void (^hello)(void) = ^(void) {
-        printf("Hello, block!\n");
-    };
+
+char stringToParse[50] = {0};                  // init all to 0
+printf("Input: ");
+scanf("%s", stringToParse);                    // read and format into the str buffer
+printf("The string to parse is: %s\n", stringToParse);    // print buffer
+
+    parseBooleanExpression();
     hello();
 
     [pool drain];
